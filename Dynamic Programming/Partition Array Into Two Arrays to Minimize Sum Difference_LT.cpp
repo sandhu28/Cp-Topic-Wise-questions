4767dbafc5422633f93ba1b4d8a926d3747bd073
Your Name <you@example.com>
2022-02-08 15:45:20 +0530

    new ques

---
+++ Dynamic Programming/Partition Array Into Two Arrays to Minimize Sum Difference_LT.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ppi pair<int,int>
+#define lli long long int
+#define li long int
+#define vi vector<int>
+#define vlli vector<lli>
+#define vli vector<li>
+#define vvi vector<vector<int>>
+#define vppi vector<pair<int,int>>
+#define rep(i,k,n) for (i = k; i < n; ++i)
+#define Repr(i,k,n) for (i = k; i >= n; --i)
+#define c(n) cout<<n<<endl
+#define z(n) cout<<n<<" "
+#define nl cout<<endl;
+#define INF INT_MAX
+using namespace std;
+
+int main(){
+    
+    
+    return 0;
+}
